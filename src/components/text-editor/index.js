867d27874e4884b643da59536e2af6d11ab5ba2e
Ефремов Георгий Станиславович <g.efremov@t1-group.ru>
2023-10-31 10:19:22 +0000

    fix

--- src/components/text-editor/index.js
+++ src/components/text-editor/index.js
@@ -1,26 +1,41 @@
 import { TEXT_FIELD_PROPS } from '@/app/constants';
-import React from 'react';
-import ReactQuill from 'react-quill';
+
+import dynamic from 'next/dynamic';
+import React, { useRef } from 'react';
 import 'react-quill/dist/quill.snow.css';
 
-export const TextEditorWrapper = ({ label, textEditorValue, handleChangeText, height= 200 }) => {
+const ReactQuillBase = dynamic(
+	async () => {
+		const { default: RQ } = await import('react-quill');
+		function QuillJS({ forwardedRef, ...props }) {
+			return <RQ ref={forwardedRef} {...props}
+			           formats={TEXT_FIELD_PROPS.formats}
+			           modules={TEXT_FIELD_PROPS.modules}
+			/>;
+		}
+
+		return QuillJS;
+	},
+	{
+		ssr: false,
+	},
+);
+
+export function TextEditor({ label, textEditorValue, handleChangeText, height = 200 }) {
+	const quillRef = useRef(null);
+	console.log(quillRef.current?.getEditor());
 	return (
 		<>
 			<h3>{label}</h3>
 			<div style={{
-				height: `${height}px`
+				height: `${height}px`,
 			}}>
-				<ReactQuill
-					formats={TEXT_FIELD_PROPS.formats}
-					modules={TEXT_FIELD_PROPS.modules}
-					onChange={handleChangeText}
-					defaultValue={textEditorValue}
+				<ReactQuillBase forwardedRef={quillRef}
+				                onChange={handleChangeText}
+				                defaultValue={textEditorValue}
 				/>
 			</div>
 
 		</>
-
 	);
-};
-
-export const TextEditor = TextEditorWrapper;
+}
