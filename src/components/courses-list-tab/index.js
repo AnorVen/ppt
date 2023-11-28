ca7021108acbf9ce2d288df5493e6012f62f4cc8
Ефремов Георгий Станиславович <g.efremov@t1-group.ru>
2023-11-28 12:21:23 +0000

    fix main text

--- src/components/courses-list-tab/index.js
+++ src/components/courses-list-tab/index.js
@@ -1,12 +1,4 @@
 'use client';
-import { CREATE_COURSE, CREATE_SEMINAR } from '@/components/actions';
-import AdditionalProgram from '@/components/courses-tab/additional-program';
-import BaseCourse from '@/components/courses-tab/BaseCourse';
-import MasterCourse from '@/components/courses-tab/master-course';
-import Seminars from '@/components/courses-tab/seminars';
-import { sagaActions } from '@/components/sagaActions';
-import { types } from '@/constants';
-import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import './style.scss';
